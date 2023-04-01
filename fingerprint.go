66ad9c530e31642986e81d7960ce58cc21fe2758
karalef <mgsiask@gmail.com>
2023-04-01 23:55:39 +0300

    add keyset id; fingerprint of full keyset

--- fingerprint.go
+++ fingerprint.go
@@ -2,27 +2,49 @@ package quark
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 )
 
-func PublicFingerprint(p PublicKeyset) Fingerprint {
-	return FingerprintOf(p.SignPublicKey().Bytes())
+// KeysetIDOf returns keyset ID of a public keyset.
+func KeysetIDOf(p PublicKeyset) KeysetID {
+	fp := FingerprintOf(p)
+	return KeysetID(fp[:8])
 }
 
-func FingerprintOf(b []byte) Fingerprint {
+// KeysetIDBytes returns keyset ID of a byte slice.
+func KeysetIDBytes(b []byte) KeysetID {
+	fp := FingerprintBytes(b)
+	return KeysetID(fp[:8])
+}
+
+// FingerprintOf returns fingerprint of a keyset.
+func FingerprintOf(p PublicKeyset) Fingerprint {
+	md5 := md5.New()
+	md5.Write(p.SignPublicKey().Bytes())
+	md5.Write(p.KEMPublicKey().Bytes())
+	return Fingerprint(md5.Sum(nil))
+}
+
+// FingerprintBytes returns fingerprint of a byte slice.
+func FingerprintBytes(b []byte) Fingerprint {
 	return Fingerprint(md5.Sum(b))
 }
 
+// KeysetID represents keyset ID.
+type KeysetID [8]byte
+
+func (k KeysetID) String() string {
+	return hex.EncodeToString(k[:])
+}
+
+// Fingerprint represents keyset fingerprint.
 type Fingerprint [md5.Size]byte
 
 func (f Fingerprint) String() string {
-	return formatFP(f[:])
-}
-
-func formatFP(fp []byte) string {
 	const hex = "0123456789abcdef"
-	f := make([]byte, 0, len(fp)*3)
-	for i := 0; i < len(fp); i++ {
-		f = append(f, hex[fp[i]>>4], hex[fp[i]&0xf], ':')
+	buf := make([]byte, 0, len(f)*3)
+	for i := 0; i < len(f); i++ {
+		buf = append(buf, hex[f[i]>>4], hex[f[i]&0xf], ':')
 	}
-	return string(f[:len(f)-1])
+	return string(buf[:len(buf)-1])
 }
