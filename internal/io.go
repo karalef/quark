2d3484b23e8a90598a52b03649690d16be4872fe
karalef <mgsiask@gmail.com>
2023-05-28 17:54:26 +0300

    remove unused func

--- internal/io.go
+++ internal/io.go
@@ -1,20 +1,5 @@
 package internal
 
-import "io"
-
-// WriteFull writes b to w in one operation.
-// If a write accepted fewer bytes than requested io.ErrShortWrite is returned.
-func WriteFull(w io.Writer, b []byte) error {
-	n, err := w.Write(b)
-	if err != nil {
-		return err
-	}
-	if n != len(b) {
-		return io.ErrShortWrite
-	}
-	return nil
-}
-
 // Copy copies b to a new slice.
 func Copy(b []byte) []byte {
 	cp := make([]byte, len(b))
