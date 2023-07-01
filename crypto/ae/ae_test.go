43a145ecf7491a83defbd919b43d8f6a65140551
karalef <mgsiask@gmail.com>
2023-07-01 21:28:03 +0300

    add ae and password tests

---
+++ crypto/ae/ae_test.go
@@ -0,0 +1,41 @@
+package ae
+
+import (
+	mathrand "math/rand"
+	"testing"
+	"time"
+
+	"github.com/karalef/quark/crypto"
+	"github.com/karalef/quark/crypto/cipher"
+	"github.com/karalef/quark/crypto/mac"
+	"github.com/karalef/quark/crypto/xof"
+)
+
+func TestPassword(t *testing.T) {
+	noncryptoRand := mathrand.New(mathrand.NewSource(time.Now().UnixNano()))
+	testScheme := Build(EncryptThanMAC, cipher.AESCTR128, mac.BLAKE2b128, xof.Shake128)
+	testData := []byte("testing data")
+	testSecret, _ := crypto.RandRead(noncryptoRand, NormalSecretSize(testScheme))
+	testIV, _ := crypto.RandRead(noncryptoRand, testScheme.Cipher().IVSize())
+
+	encryptedBuffer := make([]byte, len(testData))
+	decryptedBuffer := make([]byte, len(testData))
+
+	encrypter, err := testScheme.Encrypter(testSecret, testIV)
+	if err != nil {
+		t.Fatal(err)
+	}
+	encrypter.Crypt(encryptedBuffer, testData)
+	mac1 := encrypter.MAC()
+
+	decrypter, err := testScheme.Decrypter(testSecret, testIV)
+	if err != nil {
+		t.Fatal(err)
+	}
+	decrypter.Crypt(decryptedBuffer, encryptedBuffer)
+	mac2 := decrypter.MAC()
+
+	if !mac.Equal(mac1, mac2) {
+		t.Fatal("MAC mismatch")
+	}
+}
---
+++ crypto/password/password_test.go
@@ -0,0 +1,52 @@
+package password
+
+import (
+	mathrand "math/rand"
+	"testing"
+	"time"
+
+	"github.com/karalef/quark/crypto"
+	"github.com/karalef/quark/crypto/ae"
+	"github.com/karalef/quark/crypto/cipher"
+	"github.com/karalef/quark/crypto/kdf"
+	"github.com/karalef/quark/crypto/mac"
+	"github.com/karalef/quark/crypto/xof"
+)
+
+func TestPassword(t *testing.T) {
+	noncryptoRand := mathrand.New(mathrand.NewSource(time.Now().UnixNano()))
+	testScheme := Build(
+		ae.Build(ae.EncryptThanMAC, cipher.AESCTR128, mac.BLAKE2b128, xof.Shake128),
+		kdf.Argon2i,
+	)
+	testPassword := "password"
+	testData := []byte("testing data")
+	testIV, _ := crypto.RandRead(noncryptoRand, testScheme.AE().Cipher().IVSize())
+	testSalt, _ := crypto.RandRead(noncryptoRand, ae.NormalSecretSize(testScheme.AE()))
+	testKDFParams := kdf.Argon2Params{
+		Rounds:  1,
+		Memory:  8 * 1024,
+		Threads: 1,
+	}
+
+	encryptedBuffer := make([]byte, len(testData))
+	decryptedBuffer := make([]byte, len(testData))
+
+	encrypter, err := testScheme.Encrypter(testPassword, testIV, testSalt, testKDFParams)
+	if err != nil {
+		t.Fatal(err)
+	}
+	encrypter.Crypt(encryptedBuffer, testData)
+	mac1 := encrypter.MAC()
+
+	decrypter, err := testScheme.Decrypter(testPassword, testIV, testSalt, testKDFParams)
+	if err != nil {
+		t.Fatal(err)
+	}
+	decrypter.Crypt(decryptedBuffer, encryptedBuffer)
+	mac2 := decrypter.MAC()
+
+	if !mac.Equal(mac1, mac2) {
+		t.Fatal("MAC mismatch")
+	}
+}
