7badcedf1ac0e554561da64e09549ec87cd8fe8d
karalef <mgsiask@gmail.com>
2023-07-01 21:07:22 +0300

    improve crypto/password

--- crypto/password/password.go
+++ crypto/password/password.go
@@ -11,16 +11,30 @@ type Scheme interface {
 	AE() ae.Scheme
 	KDF() kdf.KDF
 
-	// Encrypter returns AE in encryption mode.
-	// Panics if iv is not of length Cipher().IVSize().
-	Encrypter(password string, iv, salt []byte, params kdf.Params) (ae.AE, error)
+	// Encrypter returns Crypter in encryption mode.
+	// Panics if iv is not of length AE().Cipher().IVSize().
+	Encrypter(password string, iv, salt []byte, params kdf.Params) (Crypter, error)
 
-	// Decrypter returns AE in decryption mode.
-	// Panics if iv is not of length Cipher().IVSize().
-	Decrypter(password string, iv, salt []byte, params kdf.Params) (ae.AE, error)
+	// Decrypter returns Crypter in decryption mode.
+	// Panics if iv is not of length AE().Cipher().IVSize().
+	Decrypter(password string, iv, salt []byte, params kdf.Params) (Crypter, error)
 }
 
 // DeriveKey derives a key from a password and salt.
 func DeriveKey(s Scheme, password string, salt []byte, params kdf.Params) ([]byte, error) {
 	return s.KDF().Derive([]byte(password), salt, ae.NormalSecretSize(s.AE()), params)
 }
+
+// Crypter represents en/decrypter.
+type Crypter interface {
+	Scheme() Scheme
+	Crypt(dst, src []byte)
+	MAC() []byte
+}
+
+type crypter struct {
+	scheme Scheme
+	ae.AE
+}
+
+func (c crypter) Scheme() Scheme { return c.scheme }
